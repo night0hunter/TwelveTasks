c231e1e4677744b07911a23e18b8aa259cbfefd4
VarvaraGomonyuk <varvaragomonyuk@yandex.ru>
2021-04-15 17:21:55 +0300

    task four

--- qwer.py
+++ qwer.py
@@ -4,13 +4,13 @@ import pygame, requests, sys, os
 api_server = "http://static-maps.yandex.ru/1.x/"
 
 
-def load_map(scale, lon, lat):
-    lon, lat = lon, lat
+def load_map(scale, lon, lat, view):
+    lon, lat, view = lon, lat, view
     delta = str(scale)
     params = {
         "ll": ",".join([str(lon), str(lat)]),
         "spn": ",".join([delta, delta]),
-        "l": "map"
+        "l": view
     }
     response = requests.get(api_server, params=params)
     if not response:
@@ -28,7 +28,7 @@ def load_map(scale, lon, lat):
     return map
 
 
-def page_up(scale):
+def page_up(scale, view):
     if scale <= 0.31:
         if scale == 0.001:
             scale += 0.001
@@ -40,11 +40,11 @@ def page_up(scale):
             scale += 0.005
         elif scale == 0.011:
             scale += 0.02
-    map = load_map(scale, 0, 0)
+    map = load_map(scale, 0, 0, view)
     return map, scale
 
 
-def page_down(scale):
+def page_down(scale, view):
     if scale >= 0.001:
         if scale == 0.002:
             scale -= 0.001
@@ -56,7 +56,7 @@ def page_down(scale):
             scale = round(scale - 0.005, 3)
         elif scale == 0.031:
             scale -= 0.02
-    map = load_map(scale)
+    map = load_map(scale, 0, 0, view)
     return map, scale 
 
 
@@ -67,31 +67,38 @@ def main():
     lon = 37.481338 
     lat = 55.669913
     scale = 0.002
-    map = load_map(scale, lon, lat)
+    view = "map"
+    map = load_map(scale, lon, lat, view)
     screen.blit(pygame.image.load(map), (0, 0))
     while running:
         event = pygame.event.wait()
         if event.type == pygame.QUIT:
            running = False
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_PAGEUP:
-                new_map, scale = page_up(scale)
+            if event.key == pygame.K_EQUALS: #page_up
+                new_map, scale = page_up(scale, view)
                 screen.blit(pygame.image.load(new_map), (0, 0))
-            if event.key == pygame.K_PAGEDOWN:
-                new_map, scale = page_down(scale)
+            if event.key == pygame.K_MINUS: #page_down
+                new_map, scale = page_down(scale, view)
                 screen.blit(pygame.image.load(new_map), (0, 0))
             if event.key == pygame.K_RIGHT:
                 lon += 0.001
-                screen.blit(pygame.image.load(load_map(scale, lon, lat)), (0, 0))
+                screen.blit(pygame.image.load(load_map(scale, lon, lat, view)), (0, 0))
             if event.key == pygame.K_LEFT:
                 lon -= 0.001
-                screen.blit(pygame.image.load(load_map(scale, lon, lat)), (0, 0))
+                screen.blit(pygame.image.load(load_map(scale, lon, lat, view)), (0, 0))
             if event.key == pygame.K_UP:
                 lat += 0.001
-                screen.blit(pygame.image.load(load_map(scale, lon, lat)), (0, 0))
+                screen.blit(pygame.image.load(load_map(scale, lon, lat, view)), (0, 0))
             if event.key == pygame.K_DOWN:
                 lat -= 0.001
-                screen.blit(pygame.image.load(load_map(scale, lon, lat)), (0, 0))
+                screen.blit(pygame.image.load(load_map(scale, lon, lat, view)), (0, 0))
+            if event.key == pygame.K_m: #кнопка с латинской буквой m
+                view = "map"
+                screen.blit(pygame.image.load(load_map(scale, lon, lat, view)), (0, 0))
+            if event.key == pygame.K_s:
+                view = "sat" # Кнопка с латинской буквой s
+                screen.blit(pygame.image.load(load_map(scale, lon, lat, view)), (0, 0))
         pygame.display.flip()
     pygame.quit()
     os.remove(map) 
